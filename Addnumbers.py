e3e4ea7c5cdd5fa860635f84656fe2201365f9b6
Shubham Kumar <shubham.kumar@AD.SAASFOCUS.COM>
2020-05-23 22:36:40 +0530

    set list and dicts

--- Addnumbers.py
+++ Addnumbers.py
@@ -2,3 +2,14 @@ x=2
 y=3
 sum=x+y
 print('sum is {0}'.format(sum))
+
+string='abcdef'
+name='Din esh'
+re=100/777
+print('revese string is {0} \nsum is {1} \nlength is {2}\n Upper case {3}'.format(string[4:1:-1],sum,len(string),string.upper()))
+print('Split {0}'.format(name.split()))
+print("result {r:1.4f}".format(r=re))
+my_list=[1,'abc',3.4,1,'abc']
+my_dict={'a':1,'b':3}
+
+print('list is {0} \ndict is {1} \nset is {2}'.format(my_list,my_dict.items(),set(my_list)))
