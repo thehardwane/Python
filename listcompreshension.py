d7ee88e005b51a828333912c541bfae0ae48bee3
Shubham Kumar <shubham.kumar@AD.SAASFOCUS.COM>
2020-05-26 02:45:27 +0530

    comprehension using if if else and nested loops

--- listcompreshension.py
+++ listcompreshension.py
@@ -11,6 +11,6 @@ mylist=[num if num%2==0 else'odd' for num in range(1,10) ]
 print(f'list {mylist}')
 
 #list comprehension using nested loops
-mylist=[num*num1 for num in [1,2,3] for num1 in [10,20,30]]
+mylist=[num*num1 for num in [1,2,3] for num1 in [10,20,300]]
 print(f'list {mylist}')
 
