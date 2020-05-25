5605029b26ce8ccd5268dfac0fa8b9e7af8eeea6
Shubham Kumar <shubham.kumar@AD.SAASFOCUS.COM>
2020-05-26 02:44:29 +0530

    nested loops

--- loopsAndstatement.py
+++ loopsAndstatement.py
@@ -6,3 +6,13 @@ for x in num:
     #print('{}'.format(constant))
 
 print('{}'.format(constant))
+
+#nested loop
+
+mylist=[ ]
+for x in[1,2,3]:
+    for y in[200,300,100]:
+        mylist.append(x*y)
+       
+ 
+print('list {}'.format(mylist))
